a5bab85e594b12673b4eb1025d229bc41806279b
jintao.deng <jintao.deng@qunar.com>
2019-03-10 20:28:17 +0800

    start

---
+++ demo/src/main/java/com/deng/NIO/EchoClient.java
@@ -0,0 +1,47 @@
+package com.deng.NIO;
+
+import io.netty.bootstrap.Bootstrap;
+import io.netty.channel.ChannelFuture;
+import io.netty.channel.ChannelInitializer;
+import io.netty.channel.ChannelOption;
+import io.netty.channel.ChannelPipeline;
+import io.netty.channel.EventLoopGroup;
+import io.netty.channel.nio.NioEventLoopGroup;
+import io.netty.channel.socket.SocketChannel;
+import io.netty.channel.socket.nio.NioSocketChannel;
+
+/**
+ * Created by deng on 19-3-10.
+ */
+public class EchoClient {
+
+    private static final String HOST="";
+    private static final int PORT=0;
+    public static void main(String[] args) {
+        EventLoopGroup group = new NioEventLoopGroup();
+        try {
+            Bootstrap b = new Bootstrap();
+            b.group(group)
+                    .channel(NioSocketChannel.class)
+                    .option(ChannelOption.TCP_NODELAY, true)
+                    .handler(new ChannelInitializer<SocketChannel>() {
+                        @Override
+                        public void initChannel(SocketChannel ch) throws Exception {
+                            ChannelPipeline p = ch.pipeline();
+                            p.addLast(new EchoClientHandler());
+                        }
+                    });
+
+            // Start the client.
+            ChannelFuture f = b.connect(HOST, PORT).sync();
+
+            // Wait until the connection is closed.
+            f.channel().closeFuture().sync();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } finally {
+            // Shut down the event loop to terminate all threads.
+            group.shutdownGracefully();
+        }
+    }
+}
---
+++ demo/src/main/java/com/deng/NIO/EchoClientHandler.java
@@ -0,0 +1,9 @@
+package com.deng.NIO;
+
+import io.netty.channel.ChannelOutboundHandlerAdapter;
+
+/**
+ * Created by deng on 19-3-10.
+ */
+public class EchoClientHandler extends ChannelOutboundHandlerAdapter {
+}
