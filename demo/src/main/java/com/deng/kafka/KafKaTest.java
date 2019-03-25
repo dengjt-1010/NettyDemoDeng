2453cd89d16fb4fc4b4ce07d79efe3b33aa841a1
jintao.deng <jintao.deng@qunar.com>
2019-03-25 12:29:24 +0800

    d

---
+++ demo/src/main/java/com/deng/kafka/KafKaTest.java
@@ -0,0 +1,13 @@
+package com.deng.kafka;
+
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerRecord;
+
+/**
+ * Created by deng on 19-3-23.
+ */
+public class KafKaTest {
+    public static void main(String[] args) {
+        KafkaProducer producer;
+    }
+}
